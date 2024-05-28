f55f8ce105315a28f57956375ea278c0b734e964
quanln <quanln.dzt@gmail.com>
2024-05-28 09:06:04 +0700

    Add more property for catalog item

--- catalog/v2022.gen.go
+++ catalog/v2022.gen.go
@@ -32,6 +32,14 @@ type HeaderWearSize struct {
 }
 
 type HeaderWearSizes []HeaderWearSize
+
+type ShirtSize struct {
+	Size      string `json:"size"`
+	SizeClass string `json:"size_class"`
+}
+
+type ShirtSizes []ShirtSize
+
 type Brand struct {
 	Value         string `json:"value"`
 	MarketplaceId string `json:"marketplace_id"`
@@ -44,20 +52,63 @@ type ProductDescription struct {
 }
 
 type ProductColor struct {
-	Language string `json:"language_tag"`
-	Value    string `json:"value"`
+	Language          string    `json:"language_tag"`
+	StandardizedColor []*string `json:"standardized_values,omitempty"`
+	Value             string    `json:"value"`
 }
+type Size struct {
+	Unit  string  `json:"unit"`
+	Value float64 `json:"value"`
+}
+
+type Capacities []Size
+
+type PItemSize struct {
+	Width  Size `json:"width"`
+	Height Size `json:"height"`
+}
+
+type UnitType struct {
+	Value string `json:"value"`
+}
+type Unit struct {
+	Type  UnitType `json:"type"`
+	Value float64  `json:"value"`
+}
+
+type ProductSize []PItemSize
 type ProductAttributes struct {
 	ProductDescription []*ProductDescription `json:"product_description,omitempty"`
 	ProductTypes       *ProductTypes         `json:"productTypes,omitempty"`
 	ProductColor       []*ProductColor       `json:"color,omitempty"`
 	Brands             *Brands               `json:"brand,omitempty"`
 	HeaderWearSizes    *HeaderWearSizes      `json:"headwear_size,omitempty"`
+	ShirtSizes         *ShirtSizes           `json:"shirt_size,omitempty"`
+	Capacities         *Capacities           `json:"capacity,omitempty"`
 	ProductImages      *ProductImages        `json:"images,omitempty"`
+	Unit               []*Unit               `json:"unit_count,omitempty"`
+	ProductSize        *ProductSize          `json:"item_width_height,omitempty"`
 }
 
+type BrowseClassification struct {
+	DisplayName      string `json:"displayName"`
+	ClassificationId string `json:"classificationId,omitempty"`
+}
+type ProductSummary struct {
+	MarketplaceId        string                `json:"marketplaceId"`
+	AdultProduct         *bool                 `json:"adultProduct,omitempty"`
+	Autographed          *bool                 `json:"autographed,omitempty"`
+	BrowseClassification *BrowseClassification `json:"browseClassification,omitempty"`
+	Color                string                `json:"color,omitempty"`
+	ItemClassification   *string               `json:"itemClassification,omitempty"`
+	ItemName             *string               `json:"itemName,omitempty"`
+	Memorabilia          *bool                 `json:"memorabilia,omitempty"`
+	Size                 *string               `json:"size,omitempty"`
+	Style                *string               `json:"style,omitempty"`
+}
 type V2022GetCatalogItemResponse struct {
-	Attributes   ProductAttributes `json:"attributes"`
-	Images       *ProductImages    `json:"images,omitempty"`
-	ProductTypes *ProductTypes     `json:"productTypes,omitempty"`
+	Attributes     ProductAttributes `json:"attributes"`
+	Images         *ProductImages    `json:"images,omitempty"`
+	ProductTypes   *ProductTypes     `json:"productTypes,omitempty"`
+	ProductSummary *[]ProductSummary `json:"summaries,omitempty"`
 }
